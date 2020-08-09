c34f73ae295f094adff0a06ed3071c5f3e502bf2
Siddhi Shirke <60963725+SiddhiShirke@users.noreply.github.com>
2020-08-09 20:00:28 +0530

    Add files via upload

---
+++ dequeue.py
@@ -0,0 +1,50 @@
+class List:    
+    def __init__(self):
+        self.data =[1,2,3]
+        print("Original List")
+        print(self.data)
+    
+    def is_empty():
+        if len(self.data)==0:
+            return 0
+        else:
+            return 1
+
+    def dequepopleft(self):
+        if len(self.data)==0:
+             print ("Empty")
+        else:
+            print("First element",self.data[0])
+            
+            self.data.pop(0)
+            print("Removed first element")
+            print(self.data)
+           
+    def dequepopright(self):
+        if len(self.data)==0:
+             print ("Empty")
+        else:
+            print("Last  element",self.data[-1])
+            
+            self.data.pop()
+            print("Removed last element")
+            print(self.data)
+
+    def dequeaddleft(self,e):
+        print("Adding  to the right")
+        self.data.append(e)
+        print(self.data)
+        
+
+    def dequeaddright(self,e):
+        self.data.insert(0,e)
+        print("Adding to the left")
+        print(self.data)
+        #(dequeaddleft(123))
+            
+ob =List()
+ob.is_empty
+ob.dequepopleft()
+ob.dequepopright()
+ob.dequeaddleft(4)
+ob.dequeaddright(5)
